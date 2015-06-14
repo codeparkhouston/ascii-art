12105714b2ef2f64d306911042462bb8adb1f854
pandafulmanda <pandafulmanda@gmail.com>
2015-06-13 22:04:20 -0500

    use functions, not methods

--- imageUtils.py
+++ imageUtils.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 
 from IPython.html.widgets import interactive
-from IPython.display import display
 
 # This function shows us our images so we can see what we're doing to them.
 def show_image(image):
@@ -27,7 +26,11 @@ def get_image_from_url(image_url_path):
 # This will let us easily update where the url for image should come from.
 set_image = interactive(get_image_from_url, image_url_path='http://png.clipart.me/previews/a03/puppy-vector-8-39942.jpg')
 
-def get_ascii_char_from_value(ascii_chars, pixel_value):
-    range_width=256/(len(ascii_chars)-1)
-    ascii_char = ascii_chars[int(math.floor(pixel_value/range_width))]
-    return ascii_char
+def resize_image(input_image, size):
+    return input_image.resize(size)
+
+def convert_image_to_grayscale(input_image):
+    return input_image.convert('L')
+
+def get_image_pixel_data(input_image):
+    return input_image.getdata()
