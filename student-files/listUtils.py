93964a08e92f90d40cb2ab981133d932ca02ada8
pandafulmanda <pandafulmanda@gmail.com>
2015-06-25 07:59:11 -0500

    need to include utils to student files

---
+++ student-files/listUtils.py
@@ -0,0 +1,18 @@
+
+def join_list_items(list, glue = ''):
+  return glue.join(list)
+
+def add_to_list(list, item):
+  list.append(item)
+
+def reshape_list(list, width):
+  list_as_string = join_list_items(list)
+
+  shaped_list = []
+  for index in xrange(0, len(list_as_string), width):
+      list_row = list_as_string[index: index + width]
+      add_to_list(shaped_list, list_row)
+
+  shaped_string = join_list_items(shaped_list, "\n")
+
+  return shaped_string
