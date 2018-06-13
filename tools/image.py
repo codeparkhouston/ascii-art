ff12580012556643d071a46c351cc2726669a311
amanda <pandafulmanda@gmail.com>
2018-06-13 02:43:23 -0500

    include colors and more notes

--- tools/image.py
+++ tools/image.py
@@ -1,7 +1,7 @@
 from PIL import Image
-import urllib
+import urllib, warnings
 
-from skimage import io
+from skimage import io, img_as_ubyte
 from skimage.transform import rescale, resize
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
@@ -35,10 +35,17 @@ def get_image_from_url(image_url_path):
 set_image = interactive(get_image_from_url, image_url_path='http://png.clipart.me/previews/a03/puppy-vector-8-39942.jpg')
 
 def resize_image(input_image, new_width, new_height):
-    return resize(input_image, (new_height, new_width))
+    resized_image = resize(input_image, (new_height, new_width), mode = 'constant', anti_aliasing = False)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        return(img_as_ubyte(resized_image))
 
 def convert_image_to_grayscale(input_image):
-    return rgb2gray(input_image)
+    # return rgb2gray(input_image)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        return(img_as_ubyte(rgb2gray(input_image)))
+
 
 def get_image_pixel_data(input_image):
     return input_image.getdata()
@@ -60,7 +67,7 @@ def center_gray(image_values_list):
 
 def get_image_values(gray_image):
     imagelist = gray_image.flatten()
-    center_gray_to_list = center_gray(imagelist)
-    imagelist = map(center_gray_to_list, imagelist)
-    pixels_in_image = list(map(scaleto255, imagelist))
-    return(pixels_in_image)
+    # center_gray_to_list = center_gray(imagelist)
+    # imagelist = map(center_gray_to_list, imagelist)
+    # pixels_in_image = list(map(scaleto255, imagelist))
+    return(list(imagelist))
