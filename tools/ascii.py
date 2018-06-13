d826fe2eb39029c44b1224ed521e00998ca50eac
amanda <pandafulmanda@gmail.com>
2018-06-13 14:12:53 -0500

    add text2png

--- tools/ascii.py
+++ tools/ascii.py
@@ -1,6 +1,8 @@
 from ipywidgets.widgets import interactive
 from IPython.display import display
 from functools import partial
+from tools.text2png import text2png
+from pathlib import Path
 
 from tools.image import show, get_image_from_url, get_size, resize_image, convert_image_to_grayscale, get_image_values
 from tools.list import join_list_items, add_to_list, reshape_list
@@ -49,11 +51,13 @@ def make_to_ascii(image_url_path, ascii_chars, new_width=35):
 
 convert_image_to_ascii = interactive(make_to_ascii, image_url_path='http://png.clipart.me/previews/a03/puppy-vector-8-39942.jpg', ascii_chars = {'a': ASCII_A, 'b': ASCII_B, 'c': ASCII_C, 'd': ASCII_D, 'e': ASCII_E}, new_width=[20, 50, 5])
 
-def file_saver(plot, text,save_to_filename):
+def file_saver(plot, text, save_to_filename):
     plot.savefig(save_to_filename + '.png')
     ascii_text = open(save_to_filename + '.txt', 'w+')
     ascii_text.write(text)
     ascii_text.close()
+    font_path = Path('./assets/font.ttf')
+    encoded_image_path = text2png(text, save_to_filename + '-ascii.png', fontfullpath = str(font_path.resolve()), width = len(text.split('\n')[0]) * 8)
 
 def interactive_ascii():
     display(convert_image_to_ascii)
---
+++ tools/text2png.py
@@ -0,0 +1,46 @@
+# coding=utf8
+# https://gist.github.com/destan/5540702
+
+import PIL
+from PIL import ImageFont
+from PIL import Image
+from PIL import ImageDraw
+
+
+def text2png(text, fullpath, color = "#000", bgcolor = "#FFF", fontfullpath = None, fontsize = 13, leftpadding = 3, rightpadding = 3, width = 200):
+  REPLACEMENT_CHARACTER = u'\uFFFD'
+  NEWLINE_REPLACEMENT_STRING = ' ' + REPLACEMENT_CHARACTER + ' '
+  font = ImageFont.load_default() if fontfullpath == None else ImageFont.truetype(fontfullpath, fontsize)
+
+  lines = []
+  line = u""
+
+  for word in text.split():
+    if word == REPLACEMENT_CHARACTER: #give a blank line
+      lines.append( line[1:] ) #slice the white space in the begining of the line
+      line = u""
+      lines.append( u"" ) #the blank line
+    elif font.getsize( line + ' ' + word )[0] <= (width - rightpadding - leftpadding):
+      line += ' ' + word
+    else: #start a new line
+      lines.append( line[1:] ) #slice the white space in the begining of the line
+      line = u""
+
+      #TODO: handle too long words at this point
+      line += ' ' + word #for now, assume no word alone can exceed the line width
+
+  if len(line) != 0:
+    lines.append( line[1:] ) #add the last line
+
+  line_height = font.getsize(text)[1]
+  img_height = line_height * (len(lines) + 1)
+
+  img = Image.new("RGBA", (width, img_height), bgcolor)
+  draw = ImageDraw.Draw(img)
+
+  y = 0
+  for line in lines:
+    draw.text( (leftpadding, y), line, color, font=font)
+    y += line_height
+
+  img.save(fullpath)
